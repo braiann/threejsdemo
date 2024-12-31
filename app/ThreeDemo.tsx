f9ffef0fe8eda1421fec8a032ef81109f183ab76
Braian <villasantibraian@icloud.com>
2024-12-31 12:39:25 -0300

    object movement with blur

--- app/ThreeDemo.tsx
+++ app/ThreeDemo.tsx
@@ -2,6 +2,8 @@
 
 import { useEffect, useRef } from "react";
 import * as THREE from "three";
+import { BokehPass, RenderPass } from "three/examples/jsm/Addons.js";
+import { EffectComposer } from "three/examples/jsm/postprocessing/EffectComposer.js";
 
 export default function ThreeDemo() {
     const mountRef = useRef<HTMLDivElement>(null);
@@ -9,8 +11,10 @@ export default function ThreeDemo() {
     useEffect(() => {
         if (!mountRef.current) return;
 
+        // SCENE
         const scene = new THREE.Scene();
 
+        // CAMERA
         const camera = new THREE.PerspectiveCamera(
             75, // Field of view in degrees
             window.innerWidth / window.innerHeight, // Aspect ratio
@@ -19,45 +23,220 @@ export default function ThreeDemo() {
         );
         camera.position.z = 5;
 
+        // CONFIG
+        const config = {
+            zoom: {
+                minDistance: 2,
+                maxDistance: 10,
+                zoomSpeed: 0.1,
+            },
+            movement: {
+                acceleraton: 0.05,
+                friction: 0.93,
+                maxVelocity: 0.3,
+                horizontalSpeed: 0.5,
+                fadeSpeed: 0.05,
+                startX: 0,
+                endX: -5,
+                startZ: 0,
+                endZ: 3,
+                startY: 0,
+                endY: -5,
+                startYRotation: 0,
+                endYRotation: Math.PI / 4,
+                initialOpacity: 1,
+            },
+            dof: {
+                focus: 5,
+                aperture: 0.01,
+                maxBlur: 0.01,
+            },
+        };
+
+        const state = {
+            velocity: 0,
+        };
+
+        // RENDERER
         const renderer = new THREE.WebGLRenderer({
             antialias: true,
             alpha: true,
         });
         renderer.setSize(window.innerWidth, window.innerHeight);
         renderer.setPixelRatio(window.devicePixelRatio);
-
         mountRef.current.appendChild(renderer.domElement);
 
-        const material = new THREE.LineBasicMaterial({ color: 0x0000ff });
+        // Gradient Shader Material
+        const gradientMaterial = new THREE.ShaderMaterial({
+            uniforms: {
+                color1: { value: new THREE.Color(0x191e24) }, // Dark gray
+                color2: { value: new THREE.Color(0x011936) }, // Blue
+                color3: { value: new THREE.Color(0x191e24) }, // Dark gray
+            },
+            vertexShader: `
+        varying vec2 vUv;
+        void main() {
+            vUv = uv;
+            gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
+        }
+    `,
+            fragmentShader: `
+        uniform vec3 color1;
+        uniform vec3 color2;
+        uniform vec3 color3;
+        varying vec2 vUv;
+        void main() {
+            float gradient = smoothstep(0.0, 0.5, vUv.y) - smoothstep(0.5, 1.0, vUv.y);
+            vec3 color = mix(color1, color2, vUv.y) + color3 * gradient;
+            gl_FragColor = vec4(color, 1.0);
+        }
+    `,
+            side: THREE.DoubleSide,
+        });
 
-        const points = [];
-        points.push(new THREE.Vector3(-10, 0, 0));
-        points.push(new THREE.Vector3(0, 10, 0));
-        points.push(new THREE.Vector3(10, 0, 0));
+        // Create a large background plane
+        const backgroundGeometry = new THREE.PlaneGeometry(100, 100);
+        const backgroundMesh = new THREE.Mesh(
+            backgroundGeometry,
+            gradientMaterial
+        );
 
-        const geometry = new THREE.BufferGeometry().setFromPoints(points);
+        // Position the background plane
+        backgroundMesh.position.z = -10; // Place it far behind the objects
+        scene.add(backgroundMesh);
 
-        const line = new THREE.Line(geometry, material);
+        // PLANE
+        const planeGeometry = new THREE.PlaneGeometry(1, 1);
+        const planeMaterial = new THREE.MeshBasicMaterial({
+            side: THREE.DoubleSide,
+            transparent: true,
+            opacity: config.movement.initialOpacity,
+        });
+        const plane = new THREE.Mesh(planeGeometry, planeMaterial);
+        plane.position.x = config.movement.startX;
+        plane.position.z = config.movement.startZ;
+        plane.position.y = config.movement.startY;
+        plane.rotation.y = config.movement.startYRotation;
+        scene.add(plane);
+
+        // POST PROCESSING
+        const composer = new EffectComposer(renderer);
+
+        const renderPass = new RenderPass(scene, camera);
+        composer.addPass(renderPass);
+
+        const bokehPass = new BokehPass(scene, camera, {
+            focus: config.dof.focus,
+            aperture: config.dof.aperture,
+            maxblur: config.dof.maxBlur,
+        });
+        composer.addPass(bokehPass);
+
+        const textureLoader = new THREE.TextureLoader();
+        textureLoader.load(
+            "https://raw.githubusercontent.com/braiann/portfolio/refs/heads/main/resources/images/project-screenshots/gobeyond.webp",
+            (texture) => {
+                const imageAspect = texture.image.width / texture.image.height;
+
+                const planeHeight = 3;
+                const planeWidth = planeHeight * imageAspect;
+                plane.geometry.dispose();
+                plane.geometry = new THREE.PlaneGeometry(
+                    planeWidth,
+                    planeHeight
+                );
+
+                planeMaterial.map = texture;
+                planeMaterial.needsUpdate = true;
+            }
+        );
+
+        // LIGHTS
         const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
         scene.add(ambientLight);
-        scene.add(line);
 
         const directionalLight = new THREE.DirectionalLight(0xffffff, 0.5);
         directionalLight.position.set(2, 2, 2);
         scene.add(directionalLight);
 
+        // RESIZE
         function handleResize() {
+            const width = window.innerWidth;
+            const height = window.innerHeight;
+
             camera.aspect = window.innerWidth / window.innerHeight;
             camera.updateProjectionMatrix();
+
             renderer.setSize(window.innerWidth, window.innerHeight);
+            composer.setSize(width, height);
+            bokehPass.setSize(width, height);
         }
         window.addEventListener("resize", handleResize);
 
+        // CONTROLS
+        function handleWheel(event: WheelEvent) {
+            event.preventDefault();
+
+            const delta = Math.sign(event.deltaY);
+
+            state.velocity -= delta * config.movement.acceleraton;
+            state.velocity = Math.max(
+                Math.min(state.velocity, config.movement.maxVelocity),
+                -config.movement.maxVelocity
+            );
+        }
+        mountRef.current.addEventListener("wheel", handleWheel, {
+            passive: false,
+        });
+
+        // ANIMATE
+        function animate() {
+            requestAnimationFrame(animate);
+
+            const newXPosition = plane.position.x - state.velocity;
+            plane.position.x = newXPosition;
+
+            const newZPosition = THREE.MathUtils.lerp(
+                config.movement.startZ,
+                config.movement.endZ,
+                (newXPosition - config.movement.startX) /
+                    (config.movement.endX - config.movement.startX)
+            );
+            plane.position.z = newZPosition;
+
+            const newYPosition = THREE.MathUtils.lerp(
+                config.movement.startY,
+                config.movement.endY,
+                (newXPosition - config.movement.startX) /
+                    (config.movement.endX - config.movement.startX)
+            );
+            plane.position.y = newYPosition;
+
+            const newYRotation = THREE.MathUtils.lerp(
+                config.movement.startYRotation,
+                config.movement.endYRotation,
+                (newXPosition - config.movement.startX) /
+                    (config.movement.endX - config.movement.startX)
+            );
+            plane.rotation.y = newYRotation;
+
+            const distanceFromCenter = Math.abs(newXPosition);
+            const maxDistance = 5;
+            const opacity = 1 - distanceFromCenter / maxDistance;
+            plane.material.opacity = Math.max(0, Math.min(1, opacity));
+
+            state.velocity *= config.movement.friction;
+
+            composer.render();
+        }
+
+        animate();
+
+        // CLEANUP
         return () => {
             window.removeEventListener("resize", handleResize);
+            mountRef.current?.removeEventListener("wheel", handleWheel);
             mountRef.current?.removeChild(renderer.domElement);
-            geometry.dispose();
-            material.dispose();
             renderer.dispose();
         };
     }, []);
--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,12 @@ import ThreeDemo from "./ThreeDemo";
 
 export default function Home() {
     return (
-        <main>
+        <main
+            style={{
+                backgroundImage:
+                    "url(https://pics.craiyon.com/2023-06-28/226114b3fe5b4633942851d940277e6f.webp)",
+            }}
+        >
             <ThreeDemo />
         </main>
     );
